b7cb72771302c496af1e15eeb638c725eb1d94d5
jumpjumpdog <jonathan52vivian@gmail.com>
2018-07-11 20:49:30 +0800

    Finish Stock Analysis easy

---
+++ src/Solution/StockAnalysis.java
@@ -0,0 +1,13 @@
+package Solution;
+
+public class StockAnalysis {
+    public int maxProfit(int[]prices) {
+        int sum = 0;
+        for(int i=1;i<prices.length;i++){
+            if(prices[i] > prices[i-1] ){
+                sum+=prices[i] = prices[i-1];
+            }
+        }
+        return sum;
+    }
+}
