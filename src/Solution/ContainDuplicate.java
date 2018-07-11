4c3bf040ae82f10ddc22a34762aecaf3b47d0808
jumpjumpdog <jonathan52vivian@gmail.com>
2018-07-11 21:54:22 +0800

    Finish  Array Contains Duplicate Problem

--- src/Solution/ContainDuplicate.java
+++ src/Solution/ContainDuplicate.java
@@ -1,4 +1,15 @@
 package Solution;
 
+import java.util.Arrays;
+
 public class ContainDuplicate {
+    public boolean containsDuplicate(int[] nums) {
+        int len =nums.length;
+           for(int i=0;i<len-1;i++)
+               for(int j=i+1;j<len;j++){
+                    if(nums[i] == nums[j])
+                        return true;
+               }
+            return false;
+    }
 }
