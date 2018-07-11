9d76938ad6107090af333b1deb82281cadebbb9a
jumpjumpdog <jonathan52vivian@gmail.com>
2018-07-11 21:59:48 +0800

    Update  Array Contains Duplicate Problem 将时间从400ms降低至5ms

--- src/Solution/ContainDuplicate.java
+++ src/Solution/ContainDuplicate.java
@@ -4,12 +4,20 @@ import java.util.Arrays;
 
 public class ContainDuplicate {
     public boolean containsDuplicate(int[] nums) {
+//        int len =nums.length;
+//           for(int i=0;i<len-1;i++)
+//               for(int j=i+1;j<len;j++){
+//                    if(nums[i] == nums[j])
+//                        return true;
+//               }
+//            return false;
+        Arrays.sort(nums);
         int len =nums.length;
-           for(int i=0;i<len-1;i++)
-               for(int j=i+1;j<len;j++){
-                    if(nums[i] == nums[j])
-                        return true;
-               }
-            return false;
+        for(int i=0;i<len-1;i++){
+            if(nums[i] == nums[i+1])
+                return true;
+        }
+        return false;
+
     }
 }
