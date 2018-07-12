7948e42619a2a4c43363443059171ae93b763e5e
jumpjumpdog <jonathan52vivian@gmail.com>
2018-07-12 13:27:00 +0800

    移动0

--- src/Main.java
+++ src/Main.java
@@ -1,16 +1,14 @@
-import Solution.ArrayRotate;
-import Solution.PlusOne;
-import Solution.SingleNumber;
-import Solution.StockAnalysis;
+import Solution.*;
 
 public class Main {
 
     public static void main(String[] args) {
 //        ArrayRotate solution = new ArrayRotate();
 //        SingleNumber solution = new SingleNumber();
-        PlusOne plusOne = new PlusOne();
-        int[]arrays = new int[]{7,2,8,5,0,9,1,2,9,5,3,6,6,7,3,2,8,4,3,7,9,5,7,7,4,7,4,9,4,7,0,1,1,1,7,4,0,0,6};
-        arrays = plusOne.plusOne(arrays);
+//        PlusOne plusOne = new PlusOne();
+        MoveZeroes moveZeroes = new MoveZeroes();
+        int[]arrays = new int[]{0,1,0,3,12};
+        moveZeroes.moveZeroes(arrays);
         for(Object o: arrays){
             System.out.println(o);
         }
---
+++ src/Solution/MoveZeroes.java
@@ -0,0 +1,15 @@
+package Solution;
+
+public class MoveZeroes {
+    public void moveZeroes(int[] nums) {
+        for(int i=nums.length-1;i > 0;i--)
+            for(int j =0;j <i ;j++){
+            if(nums[j] == 0){
+                int temp = nums[j];
+                nums[j] = nums[j+1];
+                nums[j+1] = temp;
+            }
+            }
+
+    }
+}
